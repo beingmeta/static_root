1403e3a1056929924094b531354350935d1ed121
Ken Haase <kh@beingmeta.com>
2012-06-27 09:44:09 +0000

    Tweaks to flyleaf page

--- sbooks/flyleaf.js
+++ sbooks/flyleaf.js
@@ -1,28 +1,35 @@
+/* -*- Mode: Javascript; Character-encoding: utf-8; -*- */
+
+/* Copyright (C) 2011-2012 beingmeta, inc.
+   This implements the interaction logic used by the sBooks flyleaf.  */
+
 function changePricing(evt)
 {
-    var target=fdjtUI.T(evt);
-    var input=fdjtDOM.getParent(target,'input');
-    var tbody=fdjtDOM.getParent(target,'tbody');
-    if (tbody) fdjtDOM.setClass(tbody,'ispaid',input.checked);
-    return;
+  evt=evt||event;
+  var target=fdjtUI.T(evt);
+  var input=fdjtDOM.getParent(target,'input');
+  var tbody=fdjtDOM.getParent(target,'tbody');
+  if (tbody) fdjtDOM.setClass(tbody,'ispaid',input.checked);
+  return;
 }
 
 var precString=fdjtString.precString;
 
 function changePrice(evt)
 {
-    var price_input=fdjtID("BASEPRICE");
-    var baseprice=((price_input)&&(parseFloat(price_input.value)));
-    if ((typeof baseprice === 'number')&&
-	(baseprice>0)&&(baseprice<100)) {
-	var giftdelta=Math.floor(Math.max(baseprice*0.15,1.00))-0.01;
-	var pricedelta=Math.floor(Math.max(baseprice*0.30,2.00))-0.01;
-	var giftprice=fdjtID("GIFTPRICE");
-	var purchaseprice=fdjtID("PURCHASEPRICE");
-	if (giftprice)
-	    giftprice.innerHTML=precString(baseprice+giftdelta,2);
-	if (purchaseprice)
-	    purchaseprice.innerHTML=precString(baseprice+pricedelta,2);}
-    return;
+  var price_input=fdjtID("BASEPRICE");
+  var baseprice=((price_input)&&(parseFloat(price_input.value)));
+  if ((typeof baseprice === 'number')&&
+      (baseprice>0)&&(baseprice<100)) {
+    var giftdelta=Math.floor(Math.max(baseprice*0.15,1.00))-0.01;
+    var pricedelta=Math.floor(Math.max(baseprice*0.30,2.00))-0.01;
+    var giftprice=fdjtID("GIFTPRICE");
+    var purchaseprice=fdjtID("PURCHASEPRICE");
+    if (giftprice)
+      giftprice.innerHTML=precString(baseprice+giftdelta,2);
+    if (purchaseprice)
+      purchaseprice.innerHTML=precString(baseprice+pricedelta,2);}
+  return;
 }
+
 
