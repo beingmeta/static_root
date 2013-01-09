bf5fdd15d99c03b75d8d005c04788a79a27b2c22
Ken Haase <kh@beingmeta.com>
2013-01-09 13:06:25 -0500

    Fix publish to set the right mime types

--- publish-bundle.bash
+++ publish-bundle.bash
@@ -7,7 +7,7 @@
     s3cmd put --encoding=utf-8 --mime-type=text/javascript \
 	      --add-header "Content-Encoding: gzip" \
 	      sbooks/codex.min.js.gz ${r}sbooks/bundle.js;
-    s3cmd put --encoding=utf-8 --mime-type=text/javascript \
+    s3cmd put --encoding=utf-8 --mime-type=text/css \
 	      --add-header "Content-Encoding: gzip" \
 	      sbooks/codex.css.gz ${r}sbooks/bundle.css;
     s3cmd put --encoding=utf-8 --mime-type=text/javascript \
@@ -16,7 +16,7 @@
     s3cmd put --encoding=utf-8 --mime-type=text/javascript \
 	      --add-header "Content-Encoding: gzip" \
 	      sbooks/codex.min.js.gz ${r}codexapp.js;
-    s3cmd put --encoding=utf-8 --mime-type=text/javascript \
+    s3cmd put --encoding=utf-8 --mime-type=text/css \
 	      --add-header "Content-Encoding: gzip" \
 	      sbooks/codex.css.gz ${r}codexapp.css;
 done;
