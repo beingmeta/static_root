f353dece50f5b4f7c79166b4a2e989d8d159817d
Ken Haase <kh@beingmeta.com>
2013-01-13 18:50:41 +0000

    Fixed / typo in bundle publishing

--- publish-bundle.bash
+++ publish-bundle.bash
@@ -2,14 +2,14 @@
     echo "Copying to " $r;
     s3cmd put --encoding=utf-8 --mime-type=text/javascript \
 	      --add-header "Content-Encoding: gzip"            \
-	      sbooks/codex.min.js.gz ${r}/sbooks/bundle.js;
+	      sbooks/codex.min.js.gz ${r}sbooks/bundle.js;
     s3cmd put --encoding=utf-8 --mime-type=text/css        \
 	      --add-header "Content-Encoding: gzip"            \
-	      sbooks/codex.css.gz ${r}/sbooks/bundle.css;
+	      sbooks/codex.css.gz ${r}sbooks/bundle.css;
     s3cmd put --encoding=utf-8 --mime-type=text/javascript \
 	      --add-header "Content-Encoding: gzip"            \
-	      sbooks/codex.min.js.gz ${r}/codexapp.js;
+	      sbooks/codex.min.js.gz ${r}codexapp.js;
     s3cmd put --encoding=utf-8 --mime-type=text/css        \
 	      --add-header "Content-Encoding: gzip"            \
-	      sbooks/codex.css.gz ${r}/codexapp.css;
+	      sbooks/codex.css.gz ${r}codexapp.css;
 done;
