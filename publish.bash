a97459a6907e5c437ad31b24dace8e0cc7f66e70
Ken Haase <kh@beingmeta.com>
2014-03-12 19:50:09 +0000

    Switched back to using s3cmd after the AWS CLI tanked

--- publish.bash
+++ publish.bash
@@ -1,29 +1,29 @@
 export AWS_DEFAULT_REGION=us-east-1
-COPYJSGZ="aws s3 cp --content-type=text/javascript;charset=utf8 --content-encoding=gzip --acl=public-read"
-COPYCSSGZ="aws s3 cp --content-type=text/css;charset=utf8 --content-encoding=gzip --acl=public-read"
-COPY="aws s3 cp --recursive --acl=public-read"
-SYNC="aws s3 sync --acl=public-read"
+COPYJSGZ="s3cmd put -m text/javascript;charset=utf8 --acl-public --add-header=Content-encoding:gzip"
+COPYCSSGZ="s3cmd put -m text/css;charset=utf8 --acl-public --add-header=Content-encoding:gzip"
+COPY="s3cmd put --recursive --acl-public"
+SYNC="s3cmd sync --acl-public"
 
 for r in `cat ./.s3root`; do \
  echo "Copying to" ${r}; \
- aws s3 cp index.html ${r}index.html; \
- $COPYJSGZ sbooks/codex.min.js.gz ${r}sbooks/bundle.js; \
- $COPYCSSGZ sbooks/codex.css.gz ${r}sbooks/bundle.css; \
- $COPYJSGZ sbooks/codex.min.js.gz ${r}codexapp.js; \
- $COPYCSSGZ sbooks/codex.css.gz ${r}codexapp.css; \
+ s3cmd put index.html ${r}index.html; \
+ $COPYJSGZ sbooks/codex.min.js ${r}sbooks/bundle.js; \
+ $COPYCSSGZ sbooks/codex.css ${r}sbooks/bundle.css; \
+ $COPYJSGZ sbooks/codex.min.js ${r}codexapp.js; \
+ $COPYCSSGZ sbooks/codex.css ${r}codexapp.css; \
  for d in fdjt knodules codex; \
    do $SYNC --exclude="*" --include="*.js" \
-            "--content-type=text/javascript; charset=utf-8" \
-            ${d} ${r}${d};
+            -m "text/javascript;charset=utf-8" \
+            ${d} ${r}${d}/;
       $SYNC --exclude="*" --include="*.css" \
-            "--content-type=text/css; charset=utf-8" \
-	    ${d} ${r}${d};
+            -m "text/css;charset=utf-8" \
+	    ${d} ${r}${d}/;
    done;
  for g in g/codex g/sbooks g/beingmeta; \
    do $SYNC --exclude="*" \
          --include="*.png" --include="*.gif" --include="*.jpg" \
-         ${g} ${r}${g};
-      $SYNC --exclude="*" --include="*.svgz" --content-encoding=gzip \
-	    ${g} ${r}${g};
+         ${g} ${r}${g}/;
+      $SYNC --exclude="*" --include="*.svgz" --add-header=Content-encoding:gzip \
+	    ${g} ${r}${g}/;
   done;
 done;
