f7f07b69d1f80ccf7344c1df636b0bebc3f0514a
Ken Haase <kh@beingmeta.com>
2014-12-06 18:05:00 -0500

    Fix typo in print.js

--- sbooks/print.js
+++ sbooks/print.js
@@ -70,8 +70,9 @@ var CodexStaticLayout=
 	    var xwidth=geom.width, xheight=geom.height;
 	    var width=geom.inner_width, height=geom.inner_height;
 	    fdjt.Log("Body: %s",document.body.className);
-	    if (document.body.getAttribute("style")) {
-		fdjt.Log("Body style: %s",document.body.getAttribute("style"));
+	    if (document.body.getAttribute("style")) 
+		fdjt.Log("Body style: %s",
+			 document.body.getAttribute("style"));
 	    fdjt.Log("Page geometry: %j",geom);
 	    pagerule=fdjtDOM.addCSSRule(
 		"div.codexpage",
@@ -88,79 +89,84 @@ var CodexStaticLayout=
 	    addPageNumbers(layout);
 	    fdjtDOM.dropClass(document.body,"cxLAYOUT");
 	    if (fdjtState.getQuery("debug"))
-		fdjtDOM.addClass(document.body,"cxDEBUGLAYOUT");}
+		fdjtDOM.addClass(document.body,"cxDEBUGLAYOUT");
+	    fdjt.Log("Done with layout");}
 
-	function addPageNumbers(layout){
-	    var pages=layout.pages;
-	    var i=0, lim=pages.length;
-	    while (i<lim) {
-		var page=pages[i++];
-		// fdjt.Log("Page %d: %j",i,getGeometry(page));
-		var pagenum=page.getAttribute("data-pagenum");
-		if (!(pagenum)) continue;
-		page.appendChild(
-		    fdjtDOM("span.codexpagenumber",pagenum));}}
+	    function addPageNumbers(layout){
+		var pages=layout.pages;
+		var i=0, lim=pages.length;
+		while (i<lim) {
+		    var page=pages[i++];
+		    // fdjt.Log("Page %d: %j",i,getGeometry(page));
+		    var pagenum=page.getAttribute("data-pagenum");
+		    if (!(pagenum)) continue;
+		    page.appendChild(
+			fdjtDOM("span.codexpagenumber",pagenum));}}
 
-	function setOffsets(layout,height){
-	    var pages=layout.pages;
-	    var i=0, lim=pages.length;
-	    while (i<lim) {
-		var page=pages[i++], style=page.style;
-		style.position='absolute';
-		style.top=((i-1)*height)+"px";}}
-	/*
-	function setOffsets(layout,height){
-	    var pages=layout.pages;
-	    var i=0, lim=pages.length;
-	    while (i<lim) {
-		var page=pages[i++], style=page.style;
-		style.position='static';}}
-	*/
+	    function setOffsets(layout,height){
+		var pages=layout.pages;
+		var i=0, lim=pages.length;
+		while (i<lim) {
+		    var page=pages[i++], style=page.style;
+		    style.position='absolute';
+		    style.top=((i-1)*height)+"px";}}
+	    /*
+	      function setOffsets(layout,height){
+	      var pages=layout.pages;
+	      var i=0, lim=pages.length;
+	      while (i<lim) {
+	      var page=pages[i++], style=page.style;
+	      style.position='static';}}
+	    */
 
-	function updateLayout(){
-	    if (!(layout)) doLayout();
+	    function updateLayout(){
+		if (!(layout)) doLayout();
+		else {
+		    var geom=getGeometry(page,false,true);
+		    var width=geom.inner_width, height=geom.inner_height;
+		    var pages=fdjtDOM("div#CODEXPAGES");
+		    var cur=fdjtID("CODEXPAGES");
+		    cur.parentNode.replaceChild(pages,cur);
+		    layout.Revert();
+		    fdjtDOM.addClass(document.body,"cxLAYOUT");
+		    pagerule.style.width=width+"px";
+		    pagerule.style.height=height+"px";
+		    layout=new CodexLayout(
+			{container: pages,page_width:width,page_height:height,
+			 pagerule: pagerule});
+		    var children=content.childNodes, nodes=[];
+		    var i=0, lim=children.length;
+		    while (i<lim) nodes.push(children[i++]);
+		    i=0; lim=nodes.length; while (i<lim) {
+			layout.addContent(nodes[i++]);}
+		    addPageNumbers(layout);
+		    fdjtDOM.dropClass(document.body,"cxLAYOUT");}}
+
+	    var resize_timeout=false;
+	    function resize_handler(evt){
+		if (resize_timeout) clearTimeout(resize_timeout);
+		resize_timeout=setTimeout(function(){
+		    resize_timeout=false;
+		    updateLayout();},1000);}
+	    
+	    if (!(window.phantomJS)) {
+		fdjtDOM.addListener(window,"load",function(evt){
+		    doLayout();
+		    fdjtDOM.addListener(window,"resize",resize_handler);});}
 	    else {
-		var geom=getGeometry(page,false,true);
-		var width=geom.inner_width, height=geom.inner_height;
-		var pages=fdjtDOM("div#CODEXPAGES");
-		var cur=fdjtID("CODEXPAGES");
-		cur.parentNode.replaceChild(pages,cur);
-		layout.Revert();
-		fdjtDOM.addClass(document.body,"cxLAYOUT");
-		pagerule.style.width=width+"px";
-		pagerule.style.height=height+"px";
-		layout=new CodexLayout(
-		    {container: pages,page_width:width,page_height:height,
-		     pagerule: pagerule});
-		var children=content.childNodes, nodes=[];
-		var i=0, lim=children.length;
-		while (i<lim) nodes.push(children[i++]);
-		i=0; lim=nodes.length; while (i<lim) {
-		    layout.addContent(nodes[i++]);}
-		addPageNumbers(layout);
-		fdjtDOM.dropClass(document.body,"cxLAYOUT");}}
+		fdjtDOM.addListener(window,"load",function(evt){doLayout();});
+		fdjtDOM.addListener(window,"layout",function(evt){doLayout();});}
 
-	var resize_timeout=false;
-	function resize_handler(evt){
-	    if (resize_timeout) clearTimeout(resize_timeout);
-	    resize_timeout=setTimeout(function(){
-		resize_timeout=false;
-		updateLayout();},1000);}
-	
-	if (!(window.phantomJS)) {
-	    fdjtDOM.addListener(window,"load",function(evt){
-		doLayout();
-		fdjtDOM.addListener(window,"resize",resize_handler);});}
-	else fdjtDOM.addListener(window,"load",function(evt){doLayout();});
+	    document.doLayout=window.doLayout=doLayout;
+	    document.updateLayout=window.updateLayout=updateLayout;
 
-	document.doLayout=window.doLayout=doLayout;
-	document.updateLayout=window.updateLayout=updateLayout;
+	    if (getQuery("tracelevel")) {
+		var tl=parseInt(getQuery("tracelevel"));
+		if (typeof tl === "number") fdjt.CodexLayout.tracelevel=tl;}
 
-	if (getQuery("tracelevel")) {
-	    var tl=parseInt(getQuery("tracelevel"));
-	    if (typeof tl === "number") fdjt.CodexLayout.tracelevel=tl;}
+	    return {init: doLayout,
+		    update: updateLayout,
+		    getPagerule: function(){return pagerule;},
+		    getLayout: function(){return layout;}};})();
 
-	return {init: doLayout,
-		update: updateLayout,
-		getPagerule: function(){return pagerule;},
-		getLayout: function(){return layout;}};})();
+
